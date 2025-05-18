4b5020214810926d1461c1d76b7b9c4bf9d2c94c
ryan <ryangcj07.24@ichat.sp.edu.sg>
2025-05-18 14:12:34 +0800

    exercise 2 assert

--- test_Lab3.py
+++ test_Lab3.py
@@ -18,13 +18,43 @@ def test_bubble_sort_descending():
     test_arr = [90, 64, 34, 25, 22, 12, 11]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
-
+    
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
+
+def test_bubble_morethan10():
+    result=[]
+    input_arr=[64,34,25,12,22,11,90,0,0,0,0,0,0]
+    result = Lab3.bubble_sort(input_arr, 0)
+
+    assert (result == 1)
+
+def test_bubble_empty():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = []
+    result=Lab3.bubble_sort(input_arr,0)
+    assert(result == 0)
 
-    result = Lab3.bubble_sort(input_arr, 3)
 
-    assert (result == [])
+def test_bubble_notintegers():
+    result=[]
+    input_arr=[1,2.3]
+    result=Lab3.bubble_sort(input_arr,0)
+
+    assert(result==2)
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
